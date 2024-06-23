f286a2a5ec8e8784db4df07da5a30b554702f985
devora <d0556705173@gmail.com>
2024-06-23 13:09:50 +0300

    login 2 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './component/Login';
+import Welcome from './component/welcome';
+import Logon from './component/Logon';
+
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/logon" element={<Logon />} />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/" element={<Login />} /> {/* Redirect to Login by default */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
