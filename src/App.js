76ffd329530203189aae3cad3ce92393ffddc93b
ShoshiMishkovski <hand1c22@mbjcomp.org.il>
2024-06-23 10:00:47 +0300

    add design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme'; // Path to your general design theme file
+import ForgotPassword from './components/forgotPassword';
+ // Import the ForgotPassword component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    
+    </ThemeProvider>
   );
 }
 
 export default App;
+
