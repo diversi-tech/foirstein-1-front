ca65bae9c1fa7ca99b506b57d13a35ee1b33ca0d
TziviStareshefsky <t0548479993@.com>
2024-06-27 11:47:10 +0300

    addAdminRouting

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import UserManagementComponent from './components/adminEditing.jsx'
 import { store } from './redux/store.jsx';
 import { Provider } from 'react-redux';
 import { useState, useEffect } from 'react';
+import { RoutingAdmin } from './components/routingAdmin.jsx';
 
 
  // Import the ForgotPassword component
@@ -16,7 +17,7 @@ function App() {
    <Provider store={store}>
     <ThemeProvider theme={theme}>
     <div className="App">
-      <UserManagementComponent />
+      <RoutingAdmin />
     </div>
   </ThemeProvider>
    </Provider>
