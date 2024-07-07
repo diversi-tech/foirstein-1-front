f617a2f746d4ffb051426bf851ba9dae7aa8cfbc
unknown <eli762801@gmail.com>
2024-07-07 14:35:10 +0300

    ii

--- src/components/changePermission.jsx
+++ src/components/changePermission.jsx
@@ -1,111 +1,3 @@
-<<<<<<< HEAD
-=======
-// import React, { useEffect } from 'react';
-// import { ThemeProvider } from '@mui/material/styles';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { Container, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Select, MenuItem, Box } from '@mui/material';
-// import { Person as PersonIcon } from '@mui/icons-material';
-// import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
-// import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
-// import theme from '../theme';
-// import { FillData } from '../redux/actions/userAction';
-// import userService, { getAllUsers, updateUserRole } from '../axios/userAxios';
-// // -----------------------------------------------------------------------------------------------
-// const ChangePermission = () => { 
-//   const dispatch = useDispatch(); 
-//   const users = useSelector(state => state.userReducer.userList); // קבלת רשימת המשתמשים מה-Redux
-
-//   useEffect(() => { 
-//     userService.getAllUsers() 
-//       .then(response => { 
-//         dispatch(FillData(response.data)); 
-//       })
-//       .catch(error => { 
-//         console.error('Error fetching users:', error); 
-//       });
-//   }, [dispatch]); 
-//    // פונקציה לשינוי הרשאה של משתמש
-//   const handleRoleChange = (userId, newRole) => {
-//     userService.updateUserRole(userId, newRole) 
-//       .then(response => { 
-//         if (response.data.success) {
-//           dispatch(FillData(users.map(user => user.userId === userId ? { ...user, role: newRole } : user))); // עדכון הנתונים ב-Redux
-//         } else {
-//           alert('Error updating role'); 
-//         }
-//       })
-//       .catch(error => { 
-//         console.error('Error updating role:', error); 
-//       });
-//   };
-// // פונקציה להחזרת אייקון בהתאם להרשאה
-//   const getIconByRole = (role) => { 
-//     switch (role) {
-//       case 'Admin':
-//         return <GroupsOutlinedIcon fontSize="large" />;
-//       case 'Student':
-//         return <PersonIcon fontSize="large" />; 
-//       case 'Librarian':
-//         return <PersonAddAltOutlinedIcon fontSize="large" />; 
-//       default:
-//         return null;
-//     }
-//   };
-
-//   return (
-//     <ThemeProvider theme={theme}> 
-//       <Container>
-//         <Typography variant="h1" align="center">ניהול משתמשים</Typography> 
-//         <TableContainer component={Paper}>
-//           <Table>
-//             <TableHead>
-//               <TableRow>
-//                 <TableCell>שם משתמש</TableCell> 
-//                 <TableCell>סיסמה</TableCell>
-//                 <TableCell>הרשאה</TableCell> 
-//                 <TableCell></TableCell> 
-//               </TableRow>
-//             </TableHead>
-//             <TableBody>
-//               {users.map(user => ( 
-//                 <TableRow key={user.userId}> 
-//                   <TableCell>{user.username}</TableCell> 
-//                   <TableCell>{user.passwordHash}</TableCell> 
-//                   <TableCell style={{ width: '200px' }}> 
-//                     <Box display="flex" alignItems="center"> 
-//                       <Select
-//                         value={user.role} 
-//                         onChange={(e) => handleRoleChange(user.userId, e.target.value)} 
-//                         style={{ marginRight: 8, flexGrow: 1 }} 
-//                       >
-//                         <MenuItem value="Admin">מנהל</MenuItem>
-//                         <MenuItem value="Student">סטודנט</MenuItem> 
-//                         <MenuItem value="Librarian">ספרנית</MenuItem> 
-//                       </Select>
-//                       {getIconByRole(user.role)} 
-//                     </Box>
-//                   </TableCell>
-//                 </TableRow>
-//               ))}
-//             </TableBody>
-//           </Table>
-//         </TableContainer>
-//       </Container>
-//     </ThemeProvider>
-//   );
-// };
-
-
-
-
-
-
-
-
-
-
-// export default ChangePermission;
->>>>>>> elishevaFront
 import React, { useEffect } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { useDispatch, useSelector } from 'react-redux';
@@ -115,93 +7,82 @@ import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
 import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
 import theme from '../theme';
 import { FillData } from '../redux/actions/userAction';
-import userService from '../axios/userAxios';
+import userService, { getAllUsers, updateUserRole } from '../axios/userAxios';
+// -----------------------------------------------------------------------------------------------
+const ChangePermission = () => { 
+  const dispatch = useDispatch(); 
+  const users = useSelector(state => state.userReducer.userList); // קבלת רשימת המשתמשים מה-Redux
 
-const ChangePermission = () => {
-  const dispatch = useDispatch();
-  const users = useSelector(state => state.userReducer.userList);
-
-  useEffect(() => {
-    userService.getAllUsers()
-      .then(response => {
-        dispatch(FillData(response));
+  useEffect(() => { 
+    userService.getAllUsers() 
+      .then(response => { 
+        dispatch(FillData(response.data)); 
       })
-      .catch(error => {
-        console.error('Error fetching users:', error);
+      .catch(error => { 
+        console.error('Error fetching users:', error); 
       });
-  }, [dispatch]);
-
+  }, [dispatch]); 
+   // פונקציה לשינוי הרשאה של משתמש
   const handleRoleChange = (userId, newRole) => {
-    console.log('Changing role for user:', userId, 'to:', newRole); // Log role change
-    userService.updateUserRole(userId, newRole)
-      .then(response => {
-<<<<<<< HEAD
-        console.log('Role update response:', response); // Log update response
-        if (response.succes) {
-          const updatedUsers = users.map(user => 
-            user.userId === userId ? { ...user, role: newRole } : user
-          );
-          dispatch(FillData(updatedUsers));
-=======
-        if (response.success) {
-          debugger
-          dispatch(FillData(users.map(user => user.userId === userId ? { ...user, role: newRole } : user)));
->>>>>>> elishevaFront
+    userService.updateUserRole(userId, newRole) 
+      .then(response => { 
+        if (response.data.success) {
+          dispatch(FillData(users.map(user => user.userId === userId ? { ...user, role: newRole } : user))); // עדכון הנתונים ב-Redux
         } else {
-          alert('Error updating role');
+          alert('Error updating role'); 
         }
       })
-      .catch((error) => {
-        debugger;
-        console.error('Error updating role:', error);
+      .catch(error => { 
+        console.error('Error updating role:', error); 
       });
   };
-
-  const getIconByRole = (role) => {
+// פונקציה להחזרת אייקון בהתאם להרשאה
+  const getIconByRole = (role) => { 
     switch (role) {
       case 'Admin':
         return <GroupsOutlinedIcon fontSize="large" />;
       case 'Student':
-        return <PersonIcon fontSize="large" />;
+        return <PersonIcon fontSize="large" />; 
       case 'Librarian':
-        return <PersonAddAltOutlinedIcon fontSize="large" />;
+        return <PersonAddAltOutlinedIcon fontSize="large" />; 
       default:
         return null;
     }
   };
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}> 
       <Container>
-        <Typography variant="h1" align="center">ניהול משתמשים</Typography>
+        <Typography variant="h1" align="center">ניהול משתמשים</Typography> 
         <TableContainer component={Paper}>
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell align="right">הרשאה</TableCell>
-                <TableCell align="right">סיסמה</TableCell>
-                <TableCell align="right">שם משתמש</TableCell>
+                <TableCell>שם משתמש</TableCell> 
+                <TableCell>סיסמה</TableCell>
+                <TableCell>הרשאה</TableCell> 
+                <TableCell></TableCell> 
               </TableRow>
             </TableHead>
             <TableBody>
-              {users.map(user => (
-                <TableRow key={user.userId}>
-                  <TableCell align="right" style={{ width: '200px' }}>
-                    <Box display="flex" alignItems="center" justifyContent="flex-start">
-                      {getIconByRole(user.role)}
+              {users.map(user => ( 
+                <TableRow key={user.userId}> 
+                  <TableCell>{user.username}</TableCell> 
+                  <TableCell>{user.passwordHash}</TableCell> 
+                  <TableCell style={{ width: '200px' }}> 
+                    <Box display="flex" alignItems="center"> 
                       <Select
-                        value={user.role}
-                        onChange={(e) => handleRoleChange(user.userId, e.target.value)}
-                        style={{ marginLeft: 8, flexGrow: 1 }}
+                        value={user.role} 
+                        onChange={(e) => handleRoleChange(user.userId, e.target.value)} 
+                        style={{ marginRight: 8, flexGrow: 1 }} 
                       >
                         <MenuItem value="Admin">מנהל</MenuItem>
-                        <MenuItem value="Student">סטודנט</MenuItem>
-                        <MenuItem value="Librarian">ספרנית</MenuItem>
+                        <MenuItem value="Student">סטודנט</MenuItem> 
+                        <MenuItem value="Librarian">ספרנית</MenuItem> 
                       </Select>
+                      {getIconByRole(user.role)} 
                     </Box>
                   </TableCell>
-                  <TableCell align="right">{user.passwordHash}</TableCell>
-                  <TableCell align="right">{user.fname}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
@@ -212,4 +93,118 @@ const ChangePermission = () => {
   );
 };
 
+
+
+
+
+
+
+
+
+
 export default ChangePermission;
+// import React, { useEffect } from 'react';
+// import { ThemeProvider } from '@mui/material/styles';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { Container, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Select, MenuItem, Box } from '@mui/material';
+// import { Person as PersonIcon } from '@mui/icons-material';
+// import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
+// import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
+// import theme from '../theme';
+// import { FillData } from '../redux/actions/userAction';
+// import userService from '../axios/userAxios';
+
+// const ChangePermission = () => {
+//   const dispatch = useDispatch();
+//   const users = useSelector(state => state.userReducer.userList);
+
+//   useEffect(() => {
+//     userService.getAllUsers()
+//       .then(response => {
+//         dispatch(FillData(response));
+//       })
+//       .catch(error => {
+//         console.error('Error fetching users:', error);
+//       });
+//   }, [dispatch]);
+
+//   const handleRoleChange = (userId, newRole) => {
+//     console.log('Changing role for user:', userId, 'to:', newRole); // Log role change
+//     userService.updateUserRole(userId, newRole)
+//       .then(response => {
+//         console.log('Role update response:', response); // Log update response
+//         if (response.succes) {
+//           const updatedUsers = users.map(user => 
+//             user.userId === userId ? { ...user, role: newRole } : user
+//           );
+//           dispatch(FillData(updatedUsers));
+
+//         if (response.success) {
+//           debugger
+//           dispatch(FillData(users.map(user => user.userId === userId ? { ...user, role: newRole } : user)));
+//         } else {
+//           alert('Error updating role');
+//         }
+//       })
+//       .catch((error) => {
+//         debugger;
+//         console.error('Error updating role:', error);
+//       });
+//   };
+
+//   const getIconByRole = (role) => {
+//     switch (role) {
+//       case 'Admin':
+//         return <GroupsOutlinedIcon fontSize="large" />;
+//       case 'Student':
+//         return <PersonIcon fontSize="large" />;
+//       case 'Librarian':
+//         return <PersonAddAltOutlinedIcon fontSize="large" />;
+//       default:
+//         return null;
+//     }
+//   };
+
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <Container>
+//         <Typography variant="h1" align="center">ניהול משתמשים</Typography>
+//         <TableContainer component={Paper}>
+//           <Table>
+//             <TableHead>
+//               <TableRow>
+//                 <TableCell align="right">הרשאה</TableCell>
+//                 <TableCell align="right">סיסמה</TableCell>
+//                 <TableCell align="right">שם משתמש</TableCell>
+//               </TableRow>
+//             </TableHead>
+//             <TableBody>
+//               {users.map(user => (
+//                 <TableRow key={user.userId}>
+//                   <TableCell align="right" style={{ width: '200px' }}>
+//                     <Box display="flex" alignItems="center" justifyContent="flex-start">
+//                       {getIconByRole(user.role)}
+//                       <Select
+//                         value={user.role}
+//                         onChange={(e) => handleRoleChange(user.userId, e.target.value)}
+//                         style={{ marginLeft: 8, flexGrow: 1 }}
+//                       >
+//                         <MenuItem value="Admin">מנהל</MenuItem>
+//                         <MenuItem value="Student">סטודנט</MenuItem>
+//                         <MenuItem value="Librarian">ספרנית</MenuItem>
+//                       </Select>
+//                     </Box>
+//                   </TableCell>
+//                   <TableCell align="right">{user.passwordHash}</TableCell>
+//                   <TableCell align="right">{user.fname}</TableCell>
+//                 </TableRow>
+//               ))}
+//             </TableBody>
+//           </Table>
+//         </TableContainer>
+//       </Container>
+//     </ThemeProvider>
+//   );
+// };
+
+// export default ChangePermission;
