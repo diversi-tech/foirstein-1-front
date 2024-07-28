81ce6b478d3dc5712469acc4653ff78d9d8037bc
ShoshiMishkovski <hand1c22@mbjcomp.org.il>
2024-07-28 10:01:01 +0300

    ff

--- src/components/changePermission.jsx
+++ src/components/changePermission.jsx
@@ -1,17 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { ThemeProvider } from '@mui/material/styles';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  Container, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Select, MenuItem, Box, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button, TextField, InputAdornment, Checkbox
+  Container, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Select, MenuItem, Box, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button, TextField, InputAdornment, Checkbox,
+  createTheme
 } from '@mui/material';
 import { Person as PersonIcon, Search as SearchIcon } from '@mui/icons-material';
 import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
 import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
+import { CacheProvider, ThemeProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { prefixer } from 'stylis';
 import theme from '../theme';
 import { FillData } from '../redux/actions/userAction';
 import userService from '../axios/userAxios';
 import ActivityLogService from '../axios/ActivityLogAxios';
 import { getUserIdFromTokenid } from './decipheringToken';
+
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+const customTheme = (outerTheme) =>
+  createTheme({
+    direction: 'rtl',
+    palette: {
+      mode: outerTheme.palette.mode,
+    },
+  });
 
 const ChangePermission = () => {
   const dispatch = useDispatch();
@@ -142,128 +159,135 @@ const ChangePermission = () => {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <Typography variant="h1" align="center">שינוי הרשאות</Typography>
-        <Box sx={{ mb: 2 }}>
-          <TextField
-            fullWidth
-            placeholder="חפש לפי שם משתמש, תעודת זהות או הרשאה"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Box>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell align="right">הרשאה</TableCell>
-                <TableCell align="right">שם פרטי</TableCell>
-                <TableCell align="right">תעודת זהות</TableCell>
-                <TableCell align="right">הרשאות נוכחיות</TableCell>
-                <TableCell align="right">פעולות</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {filteredUsers.map(user => (
-                <TableRow key={user.userId}>
-                  <TableCell align="right" style={{ width: '200px' }}>
-                    <Box display="flex" alignItems="center" justifyContent="flex-start">
-                      {getIconByRole(user.role)}
-                      <Select
-                        value={user.role}
-                        onChange={(e) => handleRoleChange(user.userId, e.target.value)}
-                        style={{ marginLeft: 8, flexGrow: 1 }}
-                      >
-                        <MenuItem value="Admin">מנהל</MenuItem>
-                        <MenuItem value="Student">סטודנט</MenuItem>
-                        <MenuItem value="Librarian">ספרנית</MenuItem>
-                      </Select>
-                    </Box>
-                  </TableCell>
-                  <TableCell align="right">{user.fname}</TableCell>
-                  <TableCell align="right">{user.tz}</TableCell>
-                  <TableCell align="right">
-                    <Typography style={{ color: 'red' }}>
-                      {user.role === 'Librarian' && getUserPermissions(user.userId).join(', ')}
-                    </Typography>
-                  </TableCell>
-                  <TableCell align="right">
-                    {user.role === 'Librarian' && (
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={() => handlePermissionsChange(user.userId)}
-                      >
-                        שנה הרשאות
-                      </Button>
-                    )}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-
-        <Dialog
-          open={openRoleDialog}
-          onClose={() => setOpenRoleDialog(false)}
-          aria-labelledby="role-dialog-title"
-          aria-describedby="role-dialog-description"
-        >
-          <DialogTitle id="role-dialog-title" style={{ fontSize: '24px', color: 'red', textShadow: '1px 1px 2px rgba(255, 0, 0, 0.7)' }}>
-            אישור שינוי הרשאה
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="role-dialog-description" style={{ fontSize: '18px', color: 'black', textAlign: 'center' }}>
-              האם אתה בטוח כי ברצונך לשנות ל{selectedUser?.fname} את ההרשאה ל{getRoleNameInHebrew(newRole)}
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={confirmRoleChange} color="primary" autoFocus>
-              אשר
-            </Button>
-          </DialogActions>
-        </Dialog>
-
-        <Dialog
-          open={openPermissionsDialog}
-          onClose={() => setOpenPermissionsDialog(false)}
-          aria-labelledby="permissions-dialog-title"
-          aria-describedby="permissions-dialog-description"
-        >
-          <DialogTitle id="permissions-dialog-title">שינוי הרשאות</DialogTitle>
-          <DialogContent>
-            <DialogContentText id="permissions-dialog-description">
-              סמן את ההרשאות שברצונך להקצות לספרנית:
-            </DialogContentText>
-            <Box>
-              {allPermissions.map(permission => (
-                <Box key={permission}>
-                  <Checkbox
-                    checked={newPermissions.includes(permission)}
-                    onChange={() => handlePermissionToggle(permission)}
-                  />
-                  {permission}
-                </Box>
-              ))}
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={customTheme}>
+        <div dir="rtl">
+          <Container>
+            <Typography variant="h1" align="center">שינוי הרשאות</Typography>
+            <Box sx={{ mb: 2 }}>
+              <TextField
+                fullWidth
+                placeholder="חפש לפי שם משתמש, תעודת זהות או הרשאה"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </Box>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => setOpenPermissionsDialog(false)} color="primary">
-              סגור
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </Container>
-    </ThemeProvider>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="right">הרשאה</TableCell>
+                    <TableCell align="right">שם פרטי</TableCell>
+                    <TableCell align="right">תעודת זהות</TableCell>
+                    <TableCell align="right">הרשאות נוכחיות</TableCell>
+                    <TableCell align="right">פעולות</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {filteredUsers.map(user => (
+                    <TableRow key={user.userId}>
+                      <TableCell align="right" style={{ width: '200px' }}>
+                        <Box display="flex" alignItems="center" justifyContent="flex-start">
+                          {getIconByRole(user.role)}
+                          <Select
+                            value={user.role}
+                            onChange={(e) => handleRoleChange(user.userId, e.target.value)}
+                            style={{ marginLeft: 8, flexGrow: 1 }}
+                          >
+                            <MenuItem value="Admin">מנהל</MenuItem>
+                            <MenuItem value="Student">סטודנט</MenuItem>
+                            <MenuItem value="Librarian">ספרנית</MenuItem>
+                          </Select>
+                        </Box>
+                      </TableCell>
+                      <TableCell align="right">{user.fname}</TableCell>
+                      <TableCell align="right">{user.tz}</TableCell>
+                      <TableCell align="right">
+                        <Typography style={{ color: 'red' }}>
+                          {user.role === 'Librarian' && getUserPermissions(user.userId).join(', ')}
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="right">
+                        {user.role === 'Librarian' && (
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={() => handlePermissionsChange(user.userId)}
+                          >
+                            שנה הרשאות
+                          </Button>
+                        )}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+
+            <Dialog
+              open={openRoleDialog}
+              onClose={() => setOpenRoleDialog(false)}
+              aria-labelledby="role-dialog-title"
+              aria-describedby="role-dialog-description"
+            >
+              <DialogTitle id="role-dialog-title" style={{ fontSize: '24px', color: 'red', textShadow: '1px 1px 2px rgba(255, 0, 0, 0.7)' }}>
+                אישור שינוי הרשאה
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText id="role-dialog-description" style={{ fontSize: '18px', color: 'black', textAlign: 'center' }}>
+                  האם אתה בטוח כי ברצונך לשנות ל{selectedUser?.fname} את ההרשאה ל{getRoleNameInHebrew(newRole)}
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={() => setOpenRoleDialog(false)} color="primary">
+                  ביטול
+                </Button>
+                <Button onClick={confirmRoleChange} color="primary" autoFocus>
+                  אישור
+                </Button>
+              </DialogActions>
+            </Dialog>
+
+            <Dialog
+              open={openPermissionsDialog}
+              onClose={() => setOpenPermissionsDialog(false)}
+              aria-labelledby="permissions-dialog-title"
+              aria-describedby="permissions-dialog-description"
+            >
+              <DialogTitle id="permissions-dialog-title">שינוי הרשאות</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="permissions-dialog-description">
+                  סמן את ההרשאות שברצונך להקצות לספרנית:
+                </DialogContentText>
+                <Box>
+                  {allPermissions.map(permission => (
+                    <Box key={permission}>
+                      <Checkbox
+                        checked={newPermissions.includes(permission)}
+                        onChange={() => handlePermissionToggle(permission)}
+                      />
+                      {permission}
+                    </Box>
+                  ))}
+                </Box>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={() => setOpenPermissionsDialog(false)} color="primary">
+                  סגור
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </Container>
+        </div>
+      </ThemeProvider>
+    </CacheProvider>
   );
 };
 
