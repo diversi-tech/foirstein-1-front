f43cce274b0f7b628e804f482348fc55748afb5d
TziviStareshefsky <t0548479993@.com>
2024-07-29 15:48:12 +0300

    zzzz

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -100,8 +100,8 @@ export const Nav = () => {
 
   const handleLogout = () => {
     document.cookie = `jwt=; path=/; domain=.foirstein.diversitech.co.il; expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
-    setIsLoggedIn(false);
     navigate('/login');
+    setIsLoggedIn(false);
     console.log('Logging out...');
   };
 
