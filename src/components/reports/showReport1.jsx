1f2b22900c21a3adf2b36ad856a34855cb62d54b
NechamaL <n0548501802@gmail.com>
2024-07-30 11:37:43 +0300

    a

--- src/components/reports/showReport1.jsx
+++ src/components/reports/showReport1.jsx
@@ -170,9 +170,9 @@ const ReportPage = () => {
       return { 
         a, 
         b, 
-        c: c ? c.split('.')[0] : c, // Trimming milliseconds from the time
+        c: c ? new Date(c).toLocaleTimeString('he-IL', { hour: '2-digit', minute: '2-digit' }) : c,
         d, 
-        e: e ? e.split('.')[0] : e  // Trimming milliseconds from the time
+        e: e ? new Date(e).toLocaleTimeString('he-IL', { hour: '2-digit', minute: '2-digit' }) : e
       };
     });
   };
