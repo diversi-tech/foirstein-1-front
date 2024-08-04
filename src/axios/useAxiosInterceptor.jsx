229299f8e4a79a1d6d12ba12fb9ed91d46f661d6
ShoshiMishkovski <hand1c22@mbjcomp.org.il>
2024-08-04 14:47:01 +0300

    ff

--- src/axios/useAxiosInterceptor.jsx
+++ src/axios/useAxiosInterceptor.jsx
@@ -3,55 +3,11 @@ import axios from 'axios';
 import { useEffect } from 'react';
 
 const useAxiosInterceptor = () => {
+  debugger
   const navigate = useNavigate();
 
-  const getCookie = (name) => {
-    // פונקציה לקבלת ערך cookie לפי שם
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-  };
-
-  const validateToken = async (token) => {
-    if (!token) return false;
-    try {
-      const response = await axios.post('http://localhost:5211/api/validate-token', { token });
-      return response.data.isValid;
-    } catch (error) {
-      console.error('Error validating token:', error);
-      return false;
-    }
-  };
-
   useEffect(() => {
-  //   const requestInterceptor = axios.interceptors.request.use(
-  //     async (config) => {
-  //       console.log('Request config:', config);
-  //       if (config && config.url && !config.url.includes('/api/Users/login') && !config.url.includes('/api/Users/password-recovery')) {
-  //         const token = getCookie('jwt');
-  //         if (token) {
-  //           const isValid = await validateToken(token);
-  //           if (isValid) {
-  //             config.headers.Authorization = `Bearer ${token}`;
-  //           } else {
-  //             console.warn('Token is not valid');
-  //             document.cookie = 'jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-  //             navigate('/login');
-  //             return Promise.reject(new Error('Invalid token'));
-  //           }
-  //         }
-  //       }
-  //       return config;
-  //     },
-  //     (error) => {
-  //       console.error('Request error:', error);
-  //       return Promise.reject(error);
-  //     }
-  //   );
-    
-
-
-    const responseInterceptor = axios.interceptors.response.use(
+    const interceptor = axios.interceptors.response.use(
       response => response,
       error => {
         if (error.response && error.response.status === 500) {
@@ -61,10 +17,9 @@ const useAxiosInterceptor = () => {
       }
     );
 
-    // ניקוי ה-interceptors בעת סיום השימוש
+    // ניקוי ה-interceptor בעת סיום השימוש
     return () => {
-      axios.interceptors.request.eject(requestInterceptor);
-      axios.interceptors.response.eject(responseInterceptor);
+      axios.interceptors.response.eject(interceptor);
     };
   }, [navigate]);
 };
